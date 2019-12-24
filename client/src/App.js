d9683b0ff0013832c4add770c1b59ff75d9c565f
LebedevaNat <natali25lebedeva@gmail.com>
2019-12-24 22:03:53 +0300

    Редактирование главной страницы

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
                 <Switch>
                 <Route
                     path='/'
+                    component={Start}
                     exact
                   />
                   <Route
