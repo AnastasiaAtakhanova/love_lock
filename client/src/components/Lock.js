dee7358b9780cd24701beac90c7e9bff3df2e4cf
atakhanova.an <atakhanova.it-11@mail.ru>
2019-12-25 08:35:06 +0300

    В базу данных отправляются характеристики замка

--- client/src/components/Lock.js
+++ client/src/components/Lock.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import '../style/auth.css';
+import {sendLockData} from "../api/SendLockData";
 
 class Lock extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            username: this.props.username
+          };
     }
-
+    clickHandler() {
+        // const username = this.state.username;
+        const person = document.getElementById("person").value;
+        const design = document.getElementById("design").value;
+        const size = document.getElementById("size").value;
+        const message = document.getElementById("message").value;
+        sendLockData(person, design, size, message);
+        console.log(person, design, size, message);
+    }
     render() {
         return (
             <div className="answer_container">
@@ -17,25 +29,19 @@ class Lock extends Component {
                 <table>
                     <tr>
                         <td>
-                            <label>
                             <img className="image" src={require('../style/img/замок.png')}/>
-                                <p>Красный</p>
-                                <input type="radio" name="status2" id = "design"></input>
-                            </label>
+                            <p>Красный</p>
+                            <input type="radio" value="red" name = "design" id="design"></input>
                         </td>
                         <td>
-                            <label>
                             <img className="image" src={require('../style/img/замок2.png')}/>
-                                <p>Синий</p>
-                                <input type="radio" name="status2" id = "design"></input>
-                            </label>
+                            <p>Синий</p>
+                            <input type="radio" value="blue" name = "design" id="design"></input>
                         </td>
                         <td>
-                            <label>
                             <img className="image" src={require('../style/img/замок3.png')}/>
-                                <p>Желтый</p>
-                                <input type="radio" name="status2" id = "color"></input>
-                            </label>
+                            <p>Желтый</p>
+                            <input type="radio" value="yellow" name = "design" id="design"></input>
                         </td>
                     </tr>
                 </table>
@@ -49,41 +55,37 @@ class Lock extends Component {
                             <td>
                                 <label>
                                     <p>Маленький</p>
-                                    <input type="radio" name="status" id = "size"></input>
+                                    <input type="radio" value="little" name = "size" id="size"></input>
                                 </label>
                             </td>
                             <td>
                                 <label>
                                     <p>Средний</p>
-                                    <input type="radio" name="status" id = "size"></input>
+                                    <input type="radio" value= "middle" name = "size" id="size"></input>
                                 </label>
                             </td>
                             <td>
                                 <label>
                                     <p>Большой</p>
-                                    <input type="radio" name="status"></input>
+                                    <input type="radio" value="big" name = "size" id="size"></input>
                                 </label>
                             </td>
                         </tr>
                     </table>
                 </div>
                 <div class="l-Title">
-                    <p>Введите имя человека, вместе с которым Вы хотите повесить замочек</p>
+                    <p>Введи имя человека, вместе с которым вы хотите повесить замочек</p>
                 </div>
                 <p>
                 <input type="text" class="form-control" id="person"></input>
                 </p>
                 <div class="l-Title">
-                    <p>Введите сообщение</p>
+                    <p>Оставь сообщение</p>
                 </div>
                 <p>
                 <input type="text" class="form-control" id="message"></input>
                 </p>
-                <div class="btn">
-                    <form>
-                        <button class="button2">Продолжить</button>
-                    </form>
-                </div>
+                <button class="button2" onClick={this.clickHandler}>Продолжить</button>
             </div>
         );
     }    
