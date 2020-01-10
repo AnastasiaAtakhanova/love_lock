d1d18a5e16071ec03d0872abf419c41d3a6e091e
AnastasiaAtakhanova <51311254+AnastasiaAtakhanova@users.noreply.github.com>
2020-01-10 13:33:37 +0300

    Update UserData.js

--- client/src/api/UserData.js
+++ client/src/api/UserData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getUserData = (username) => {
     return axios
-        .get('http://localhost:5000/api/get_user_data', {
+        .get('http://server-lock.herokuapp.com/api/get_user_data', {
             params: {username: username}
         })
         .then(res => { 
