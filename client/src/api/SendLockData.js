839f8e077741ba428347dad03542c5a694782ce5
AnastasiaAtakhanova <51311254+AnastasiaAtakhanova@users.noreply.github.com>
2020-01-10 13:32:09 +0300

    Update SendLockData.js

--- client/src/api/SendLockData.js
+++ client/src/api/SendLockData.js
@@ -9,7 +9,7 @@ export const sendLockData=(username, person, design, size, message) => {
         message: message
     };
     return axios
-        .post('http://localhost:5000/api/send_lock_data', payload)
+        .post('http://server-lock.herokuapp.com/api/send_lock_data', payload)
         .then(res => {
             console.log('Данные успешно были отправлены');
             return res.data;
