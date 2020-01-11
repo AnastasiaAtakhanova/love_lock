dfed9f0978a39728713256750c2e43e4adfd303e
AnastasiaAtakhanova <51311254+AnastasiaAtakhanova@users.noreply.github.com>
2020-01-11 10:23:05 +0300

    Update LockList.js

--- client/src/components/LockList.js
+++ client/src/components/LockList.js
@@ -1,32 +1,29 @@
 import React, {Component} from 'react';
-import LockContent from "../components/LockContent";
 import {getUserData} from "../api/UserData";
 import {delete_lock_id} from "../api/DeleteLock";
 
-var ID = 3;
+
+var ID;
+var editing = false;
 
 class LockList extends Component {
 
     constructor(props) {
         super(props);
-        
         this.state = {
-            username: '',
+            editing: false,
             data: [[]]
-
           };
     }
 
     reloadUserData = () => {
-        console.log("Запрос на получение данных..");
-        getUserData("11").then((data) => {
+        getUserData(String(localStorage.getItem('username'))).then((data) => {
             this.setState({'data': data});
         });
-        
+
     };
 
-    clickDeleteHandler = () => {
-        console.log("Запрос на удаление данных..");   
+    clickDeleteHandler = () => {  
         delete_lock_id(Number(ID)).then((data) => {
             this.setState({'data': data});
         });
@@ -36,7 +33,7 @@ class LockList extends Component {
       let cardContent = ( 
           <div id="l-Text" class="l-Text">
               <div class="design_lock">
-            <p>Пользователь: username</p>
+            <p>Пользователь: {String(localStorage.getItem('username'))}</p>
             <p>Проверить наличие замочка?</p>
             </div>
             <button type="submit" class="btn btn-danger" onClick={this.reloadUserData}>Проверить</button>
@@ -56,18 +53,19 @@ class LockList extends Component {
         else {
            
             if (this.state.data[1] === "yellow")
-            { ID = this.state.data[5]; 
+            { ID = this.state.data[4]; 
                 cardContent = (
             <div className="card" style={{width: '24rem' }}>
                 <div className="form-group">
                     <div id="l-Text" class="l-Text">    
                     <div class="design_lock">
                         <img className="image" src={require('../style/img/замок3.png')}/>
-                        <p>Пользователь: {this.state.data[0]}</p>
-                        <p>Человек: {this.state.data[2]}</p>
+                        <p>Пользователь: {localStorage.getItem('username')}</p>
+                        <p>Размер: {this.state.data[0]}</p>
+                        <p>Человек:{this.state.data[2]}</p>
                         <p>Сообщение: {this.state.data[3]}</p>
-                        <p>Размер: {this.state.data[4]}</p>
                     </div>
+                       
                         <button type="submit" class="btn btn-danger" onClick={this.clickDeleteHandler}>Удалить замочек</button>   
                   </div>
                   </div>
@@ -76,18 +74,19 @@ class LockList extends Component {
                 );
             }
             if (this.state.data[1] === "blue")
-            { ID = this.state.data[5]; 
+            { ID = this.state.data[4]; 
                 cardContent = (
             <div className="card" style={{width: '24rem' }}>
                 <div className="form-group">
                     <div id="l-Text" class="l-Text">    
                     <div class="design_lock">
                         <img className="image" src={require('../style/img/замок2.png')}/>
-                        <p>Пользователь: {this.state.data[0]}</p>
-                        <p>Человек: {this.state.data[2]}</p>
+                        <p>Пользователь: {localStorage.getItem('username')}</p>
+                        <p>Человек:{this.state.data[2]}</p>
                         <p>Сообщение: {this.state.data[3]}</p>
-                        <p>Размер: {this.state.data[4]}</p>
+                        <p>Размер: {this.state.data[0]}</p>
                     </div>
+                           
                         <button type="submit" class="btn btn-danger" onClick={this.clickDeleteHandler}>Удалить замочек</button>   
                   </div>
                   </div>
@@ -97,18 +96,19 @@ class LockList extends Component {
             }
             if (this.state.data[1] === "red")
             {
-                ID = this.state.data[5]; 
+                ID = this.state.data[4]; 
                 cardContent = (
             <div className="card" style={{width: '24rem' }}>
                 <div className="form-group">
                     <div id="l-Text" class="l-Text">    
                     <div class="design_lock">
                         <img className="image" src={require('../style/img/замок.png')}/>
-                        <p>Пользователь: {this.state.data[0]}</p>
-                        <p>Человек: {this.state.data[2]}</p>
+                        <p>Пользователь: {localStorage.getItem('username')}</p>
+                        <p>Человек:{this.state.data[2]}</p>
                         <p>Сообщение: {this.state.data[3]}</p>
-                        <p>Размер: {this.state.data[4]}</p>
+                        <p>Размер: {this.state.data[0]}</p>
                     </div>
+                       
                         <button type="submit" class="btn btn-danger" onClick={this.clickDeleteHandler}>Удалить замочек</button>   
                   </div>
                   </div>
