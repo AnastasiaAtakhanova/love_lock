ff3a4dc6dd9ea923e4c5da58aa6f03332f625a5b
AnastasiaAtakhanova <51311254+AnastasiaAtakhanova@users.noreply.github.com>
2020-01-11 10:15:50 +0300

    Update DeleteLock.js

--- client/src/api/DeleteLock.js
+++ client/src/api/DeleteLock.js
@@ -7,11 +7,9 @@ export const delete_lock_id= (id_lock) => {
     return axios
         .post('https://server-lock.herokuapp.com/api/delete_lock_id', payload)
         .then(res => {
-            console.log('Данные успешно были удалены',  res.data.data);
             return res.data.data;
         })
         .catch((error) => {
-            console.log('Данные не были успешно удалены');
             console.log(error);
         });
 };
